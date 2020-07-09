88234022a9ecbe86d6832b7cf246e995355423d4
HyogaIto <hyoga.ito@rakus.co.jp>
2020-07-09 14:46:53 +0900

    create InsertCommentController&ShowBbsController

---
+++ src/main/java/com/example/controller/InsertCommentController.java
@@ -0,0 +1,39 @@
+package com.example.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Comment;
+import com.example.form.CommentForm;
+import com.example.repository.CommentRepository;
+
+public class InsertCommentController {
+	
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	
+
+	/**
+	 * コメント投稿を行う.
+	 * 
+	 * @param form 投稿したいコメントの内容
+	 * @return リダイレクト：掲示板画面
+	 */
+	@RequestMapping("insertComment")
+	public String insertComment(@Validated CommentForm form, BindingResult result,Model model) {
+		if (result.hasErrors()) {
+			return new ShowBbsController().index(model);
+		}
+		Comment comment = new Comment();
+		BeanUtils.copyProperties(form, comment);
+		comment.setArticleId(Integer.parseInt(form.getArticleId()));
+		commentRepository.insert(comment);
+		return "redirect:/bbs/";
+	}
+	
+}
--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -1,5 +1,46 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.form.ArticleForm;
+import com.example.form.CommentForm;
+import com.example.repository.ArticleRepository;
+
+@RequestMapping("show-bbs")
 public class ShowBbsController {
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	@ModelAttribute
+	public ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
+
+	@ModelAttribute
+	public CommentForm setUpCommentForm() {
+		return new CommentForm();
+	}
+
+	/**
+	 * 掲示板画面を表示する.
+	 * 
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("")
+	public String index(Model model) {
+
+		List<Article> articles = articleRepository.findAll();
+
+		model.addAttribute("articles", articles);
+
+		return "ex-bbs";
+	}
 
 }
