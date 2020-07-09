f1c898e694ad5ec41cf009234b9dd0ac7dbc40c9
HyogaIto <hyoga.ito@rakus.co.jp>
2020-07-09 14:30:17 +0900

    Create CommentRep&CommentForm

--- src/main/java/com/example/form/CommentForm.java
+++ src/main/java/com/example/form/CommentForm.java
@@ -1,5 +1,52 @@
 package com.example.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * 入力されたコメント情報フォーム.
+ * 
+ * @author hyoga.ito
+ *
+ */
 public class CommentForm {
+	/** 記事ID */
+	private String articleId;
+	/** コメント者名 */
+	@NotBlank(message = "名前は必須です")
+	@Size(max = 30, message = "名前は30文字以内で入力してください")
+	private String name;
+	/** コメント内容 */
+	@NotBlank(message = "コメントは必須です")
+	private String content;
+
+	public String getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "CommentForm [articleId=" + articleId + ", name=" + name + ", content=" + content + "]";
+	}
 
 }
--- src/main/java/com/example/repository/CommentRepository.java
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -1,5 +1,78 @@
 package com.example.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Comment;
+
+/**
+ * コメント情報を操作するリポジトリ.
+ * 
+ * @author hyoga.ito
+ *
+ */
+@Repository
 public class CommentRepository {
+
+	/** SQL結果をオブジェクトに入れるローマッパー */
+	private final RowMapper<Comment> COMMENT_ROW_MAPPER = (rs, i) -> {
+		Comment comment = new Comment();
+		comment.setId(rs.getInt("id"));
+		comment.setName(rs.getString("name"));
+		comment.setContent(rs.getString("content"));
+		comment.setArticleId(rs.getInt("article_id"));
+		return comment;
+	};
+
+	/** SQL実行用変数 */
+	@Autowired
+	NamedParameterJdbcTemplate template;
+
+	/** テーブル名 */
+	private final String TABLE_NAME = "comments";
+
+	/**
+	 * 記事IDからコメントを検索する.
+	 * 
+	 * @param articleId 記事ID
+	 * @return コメント一覧
+	 */
+	public List<Comment> findByArticleId(Integer articleId) {
+		String sql = "select id,name,content,article_id from " + TABLE_NAME + " "
+				+ " where article_id=:articleId order by id desc;";
+		SqlParameterSource param = new MapSqlParameterSource("articleId", articleId);
+		List<Comment> comments = template.query(sql, param, COMMENT_ROW_MAPPER);
+		return comments;
+
+	}
+
+	/**
+	 * コメントを追加する.
+	 * 
+	 * @param comment 追加したいコメント
+	 */
+	public void insert(Comment comment) {
+		String sql = "insert into " + TABLE_NAME + "(name,content,article_id) values(:name,:content,:articleId);";
+		SqlParameterSource param = new MapSqlParameterSource("name", comment.getName())
+				.addValue("content", comment.getContent()).addValue("articleId", comment.getArticleId());
+		template.update(sql, param);
+	}
+
+	/**
+	 * 記事IDからコメントを削除する.
+	 * 
+	 * @param articleId 記事ID
+	 */
+	public void deleteByArticleId(int articleId) {
+		String sql = "delete from " + TABLE_NAME + " where article_id=:articleId";
+		SqlParameterSource param = new MapSqlParameterSource("articleId", articleId);
+		template.update(sql, param);
+	}
 
 }
